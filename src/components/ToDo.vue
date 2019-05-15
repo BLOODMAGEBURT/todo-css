476844a69b5761603a6ec6c11dda3d2676688ae2
xujianbo <xujianbo@vocoor.com>
2019-05-15 09:50:51 +0800

    todo-pro

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,28 +1,6 @@
 <template>
     <div class="todo">
-      <h2>welcome todo</h2>
-      <el-form :inline="true" class="demo-form-inline" :class="{'completed':item.status}" v-for="item in items" :key="item.id">
-        <el-form-item>
-          <el-checkbox :checked="item.status ? true:false" v-model="ischecked"/>
-        </el-form-item>
-        <el-form-item>
-          <el-input v-model="item.title" placeholder="请输入"/>
-        </el-form-item>
-        <el-form-item>
-          <i class="el-icon-delete" @click="delItem()"/>
-        </el-form-item>
-      </el-form>
-      <el-form :inline="true" class="demo-form-inline completed">
-        <el-form-item>
-          <el-checkbox />
-        </el-form-item>
-        <el-form-item>
-          <el-input  placeholder="请输入"/>
-        </el-form-item>
-        <el-form-item>
-          <i class="el-icon-delete" @click="delItem()"/>
-        </el-form-item>
-      </el-form>
+      <h2>welcome todo {{ msg }}</h2>
     </div>
 </template>
 
@@ -31,24 +9,10 @@ export default {
   name: 'todo',
   data () {
     return {
-      items: [
-        {
-          id: 1,
-          title: 'i am ok',
-          status: 0
-        },
-        {
-          id: 2,
-          title: '2 i am ok',
-          status: 1
-        }
-      ]
+      msg: 'hello xuBo'
     }
   },
   methods: {
-    delItem () {
-      console.log('del_item')
-    }
   }
 }
 </script>
@@ -56,12 +20,5 @@ export default {
 <style scoped>
   * {
     /*background-color: rgba(0, 0, 0, 0.1);*/
-  }
-  .todo {
-    max-width: 800px;
-    margin: 0 auto;
-  }
-  .completed {
-    background-color: #42b983;
   }
 </style>
